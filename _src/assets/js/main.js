ae699310f47ff7f8f7770efae9c34ecf18ae2979
jemrosas <jmezrosas@gmail.com>
2020-03-09 14:01:51 +0100

    Add input functions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
+//Constantes globales
+const userName = document.querySelector('.js-input-name');
+const userJob = document.querySelector('.js-input-job');
 
-console.log('>> Ready :)');
+//Funciones para obtener datos de usuario
+function getUserName() {
+  const showName = document.querySelector('.layout__title');
+  showName.innerHTML = userName.value;
+}
+
+function getUserJob() {
+  const showJob = document.querySelector('.layout__text');
+  showJob.innerHTML = userJob.value;
+}
+
+userName.addEventListener('keyup', getUserName);
+userJob.addEventListener('keyup', getUserJob);
