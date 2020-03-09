9add79b4dc091d503d3e3d084a869adc095ba656
jemrosas <jmezrosas@gmail.com>
2020-03-09 16:17:35 +0100

    Add other usar fields to js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,6 +3,10 @@
 const userName = document.querySelector('.js-input-name');
 const userJob = document.querySelector('.js-input-job');
 const userMail = document.querySelector('.js-input-mail');
+const userPhone = document.querySelector('.js-input-phone');
+const userLinkedin = document.querySelector('.js-input-linkedin');
+const userGithub = document.querySelector('.js-input-github');
+
 //Funciones para obtener datos de usuario
 function getUserName() {
   const showName = document.querySelector('.layout__title');
@@ -15,10 +19,28 @@ function getUserJob() {
 }
 
 function showMail() {
-  const userMail = document.querySelector('.js-mail-icon');
-  userMail.href = 'mailto:' + userMail.value;
+  const iconMail = document.querySelector('.js-mail-icon');
+  iconMail.href = 'mailto:' + userMail.value;
+}
+
+function showPhone() {
+  const iconPhone = document.querySelector('.js-phone-icon');
+  iconPhone.href = 'tel:' + userPhone.value;
+}
+
+function showLinkedin() {
+  const iconLinkedin = document.querySelector('.js-linkedin-icon');
+  iconLinkedin.href = userLinkedin.value;
+}
+
+function showGithub() {
+  const iconGithub = document.querySelector('.js-github-icon');
+  iconGithub.href = userGithub.value;
 }
 
 userName.addEventListener('keyup', getUserName);
 userJob.addEventListener('keyup', getUserJob);
 userMail.addEventListener('keyup', showMail);
+userPhone.addEventListener('keyup', showPhone);
+userLinkedin.addEventListener('keyup', showLinkedin);
+userGithub.addEventListener('keyup', showGithub);
